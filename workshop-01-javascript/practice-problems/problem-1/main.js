2e3ba359cb1f78d7c6b1f702cb624fd5d62fa176
Andrew Lee <asl2219@rit.edu>
2024-09-15 12:01:31 -0400

    idk what im doing

--- workshop-01-javascript/practice-problems/problem-1/main.js
+++ workshop-01-javascript/practice-problems/problem-1/main.js
@@ -1,1 +1,12 @@
-// Fill in for exercises!
+let shoppingList = [];
+
+void addItem(item);{
+  if (!shoppingList.includes(item)) {
+    shoppingList.push(item);
+    console.log(`${item} added to your shopping list.`);
+  } else {
+    console.log(`${item} is already in your shopping list.`);
+  }
+};
+
+g
